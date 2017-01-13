f82140fed907c428fee944131244089e1a62e6e7
chenruixuan <www@chenruixuan.com>
2017-01-13 14:40:06 +0800

    change json file

--- test/index.php
+++ test/index.php
@@ -7,6 +7,18 @@
 use Chenruixuan\Kafka\Kafka;
 require __DIR__ . '/../autoload.php';
 
-
+///producer
+/*
 $kafka=new Kafka("192.168.31.204");
+$kafka->setTopic("test");
+$producter=$kafka->newProducer();
+$producter->setMessage(0,"helloworld");
+*/
+$kafka=new Kafka("192.168.31.204");
+$kafka->setTopic("test");
+$kafka->setGroup("test");
+$consumer=$kafka->newConsumer();
+$rs=$consumer->getMassage(0,10);
+
+
 
